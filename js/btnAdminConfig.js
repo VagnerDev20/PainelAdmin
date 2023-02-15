bd7bdbd50dad71bf18dcdcea076c25543be4eb1d
VagnerDev20 <vagnerprogramador18@gmail.com>
2023-02-14 23:45:48 -0300

    Responsivo atualizacao

--- js/btnAdminConfig.js
+++
@@ -1,7 +0,0 @@
-//botao configuração admin
-const btnAdminConfig = document.querySelector('.btnAdminConfig');
-const modalConfig = document.getElementById("modalConfig");
-
-btnAdminConfig.addEventListener('click', () => {
-    modalConfig.classList.toggle('show');
-});

26083519d912cfa6d1d2e98f5f66e6c3883e7dfb
VagnerDev20 <vagnerprogramador18@gmail.com>
2023-02-16 01:31:43 -0300

    projeto finalizado

--- js/dropdownUser.js
+++ js/dropdownUser.js
@@ -11,3 +11,30 @@ const dropdownIcon = document.getElementById("dropdown-icon");
 dropdownIcon.addEventListener("click", function() {
   dropdownIcon.classList.toggle("rotated");
 });
+
+
+
+
+
+
+
+const dropdownBtnClose = document.querySelector('.btnAdminConfig');
+const dropdownContentClose = document.querySelector('.dropdown-content');
+
+dropdownBtnClose.addEventListener('click', () => {
+  dropdownContentClose.classList.toggle('show');
+});
+
+
+
+
+
+
+const dropdownClose = document.querySelector('.btnAdminConfig');
+const dropdownIconClose = document.getElementById("dropdown-icon");
+
+dropdownClose.addEventListener("click", function() {
+  dropdownIconClose.classList.toggle("rotated");
+});
+
+

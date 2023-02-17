f3692d28465c0484a9a60999ac08183f09790053
VagnerDev20 <vagnerprogramador18@gmail.com>
2023-02-16 23:55:26 -0300

    front end finalizado

--- js/dropdown.js
+++
@@ -1,8 +0,0 @@
-// Obtenha o botão e, quando o usuário clicar nele, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
-
-// myFunction alterna entre adicionar e remover a classe show, 
-//que é usada para ocultar e mostrar o conteúdo suspenso
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}

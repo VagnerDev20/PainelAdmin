5c0dd7724e808ee54556a187c0bfe05cf6132aaa
VagnerDev20 <vagnerprogramador18@gmail.com>
2023-02-20 21:39:54 -0300

    update

--- js/Botoes.js
+++ js/Botoes.js
@@ -1,3 +1,4 @@
+// *********HEADER***************
 //botao icone dropdow header
 function toggleDropdownContent() {
   const dropdownContent = document.querySelector(".dropdown-content");
@@ -6,19 +7,18 @@ function toggleDropdownContent() {
 
 const dropdownBtn = document.querySelector(".btnUser");
 dropdownBtn.addEventListener("click", toggleDropdownContent);
-//*******************************************
+//*****************************************************************************
 
 
-
-// rotacionar o icone
+// rotacionar o icone header
 const dropdownIcon = document.getElementById("dropdown-icon");
-function dropdownIconRotated(){
-    dropdownIcon.classList.toggle("rotated");
+function dropdownIconRotated() {
+  dropdownIcon.classList.toggle("rotated");
 }
 dropdownIcon.addEventListener("click", dropdownIconRotated);
-//*******************************************
+//*****************************************************************************
 
-
+// *********MENU PRINCIPAL***************
 // botao menu principal
 const btnMobile = document.getElementById("btn-mobile"); // pega o elemento pelo id
 
@@ -37,42 +37,116 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
-//*******************************************
+//*****************************************************************************
 
-//dropdown submenu
+
+//dropdown submenu menu principal
 document.getElementById("myBtn").onclick = function () {
   MenuDropdown();
 };
 function MenuDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-//*******************************************
+//*****************************************************************************
+
+// *********FORMULARIOS ADMIN***************
+//script overlay formulario configuração
+const openFormBtn = document.querySelector('.btnAdminConfig');
+const myForm = document.querySelector('.modalConfig');
+const overlay = document.getElementById('overlay');
+
+function openModal() {
+  myForm.classList.add('open');
+  overlay.classList.add('visible');
+  toggleDropdownContent();
+  dropdownIconRotated();
+}
+
+function closeModal() {
+  myForm.classList.remove('open');
+  overlay.classList.remove('visible');
+}
+
+openFormBtn.addEventListener('click', openModal);
+overlay.addEventListener('click', closeModal);
+
+//*****************************************************************************
 
 
 
-/*
+//escript para fechar o formulario ao clicar no esc-->
+// seleciona o formulário que você deseja controlar
+const meuElemento = document.querySelector('.modalConfig');// seleciona a classe do form
+const overlayClose = document.getElementById('overlay');
+
+function verificarClasseShow(meuElemento) {
+  if (meuElemento.classList.contains('open')) {
+    document.querySelector('.modalConfig').classList.toggle("open");
+    overlayClose.classList.remove('visible');
+  } else {
+
+  }
+}
+
+// adiciona um evento de escuta de teclado ao documento
+document.addEventListener('keydown', (event) => {
+  // verifica se a tecla "Esc" foi pressionada
+  if (event.key === 'Escape') {
+
+    verificarClasseShow(meuElemento); // chamada da função
+
+  }
+});
+//*****************************************************************************
 
 
-<!--botao configuração dropdown-->
-        <script>
-            const btnAdminConfig = document.querySelector('.btnAdminConfig');
-            const modalConfig = document.querySelector('.modalConfig');
+//script overlay formulario perfil
+const openFormBtnPerfil = document.querySelector('.btnAdminPerfil');
+const myFormPerfil = document.querySelector('.modalPerfil');
+const overlayPerfil = document.getElementById('overlayPerfil');
 
-            btnAdminConfig.addEventListener('click', () => {
-                modalConfig.classList.toggle('show');
-            });
-        </script>
+function openModalPerfil() {
+  myFormPerfil.classList.add('open');
+  overlayPerfil.classList.add('visible');
+  toggleDropdownContent();
+  dropdownIconRotated();
+}
 
-        <!--botao icone formulario configuração-->
-        <script>
-            document.querySelector('.imgFechar').onclick = function () { imgFecharForm() };
-            function imgFecharForm() {
-                document.querySelector('.modalConfig').classList.toggle("show");
-            }
-        </script>
+function closeModalPerfil() {
+  myFormPerfil.classList.remove('open');
+  overlayPerfil.classList.remove('visible');
+}
+
+openFormBtnPerfil.addEventListener('click', openModalPerfil);
+overlayPerfil.addEventListener('click', closeModalPerfil);
+//*****************************************************************************
+
+// esc form perfil
+const meuElementoPerfil = document.querySelector('.modalPerfil');// seleciona a classe do form
+const overlayClosePerfil = document.getElementById('overlayPerfil');
+
+function verificarClasseShowPerfil(meuElementoPerfil) {
+  if (meuElementoPerfil.classList.contains('open')) {
+    document.querySelector('.modalPerfil').classList.toggle("open");
+    overlayClosePerfil.classList.remove('visible');
+  } else {
+
+  }
+}
+
+// adiciona um evento de escuta de teclado ao documento
+document.addEventListener('keydown', (event) => {
+  // verifica se a tecla "Esc" foi pressionada
+  if (event.key === 'Escape') {
+
+    verificarClasseShowPerfil(meuElementoPerfil); // chamada da função
+
+  }
+});
+//*****************************************************************************
 
 
 
 
 
-*/
+
